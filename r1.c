0a474232f134a886d6341b8be5cc2a73df3f035b
asr1410 <abhishek.rawat1410@gmail.com>
2021-01-17 18:16:47 +0530

    changes

---
+++ r1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <conio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    int arr[100] = ;
+    for (int i = 0; i < 100; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+
+    return 0;
+}
